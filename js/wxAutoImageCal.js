fde0f54ddc29ce2b789467dc1f819568cd18884f
陈攀宇 <96372678+Chicksqace@users.noreply.github.com>
2023-06-25 20:30:21 +0800

    Add files via upload

---
+++ js/wxAutoImageCal.js
@@ -0,0 +1,29 @@
+function wxAutoImageCal(e) {
+    console.log(e);
+    var originalWidth = e.detail.width;
+    var originalHeight = e.detail.height;
+    var windowWidth = 0;
+    var windowHeight = 0;
+    var autoWidth = 0;
+    var autoHeight = 0;
+    var results = {};
+    wx.getSystemInfo({
+      success: function(res) {
+        // success
+        var winWidth = res.windowWidth;
+        var winHeight = res.windowHeight;
+        if(originalWidth > winWidth){
+            autoWidth = winWidth;
+            autoHeight = (autoWidth * originalHeight) / originalWidth;
+            results.imageWidth = autoWidth + 'px';
+            results.imageHeight = autoHeight + 'px';
+        }
+        else{
+            results.imageWidth = originalWidth + 'px';
+            results.imageHeight = originalHeight + 'px';
+        }
+      }
+    })
+    return results;
+}
+module.exports.wxAutoImageCal = wxAutoImageCal
